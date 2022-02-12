7f946f330e565a253a46312a2ae30b04144e0fea
Rudam26 <99458016+Rudam26@users.noreply.github.com>
2022-02-12 16:17:58 +0530

    Update w1question2.c

--- w1question2.c
+++ w1question2.c
@@ -6,7 +6,10 @@ int main()
     scanf("%d" "%d",&a,&b);
     if(a>b)
       printf("The maximum number is %d",a);
-    else
+    else if(b>a)
       printf("The maximum number is %d",b);
+    else if(b==a)
+      printf("%d and %d are both equal numbers.",a,b);
     return 0;
 }
+
